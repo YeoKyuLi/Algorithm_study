653743e3f4b1ec9914f40022a9b4c5385ea3a151
YeoKyuLi <kyuli.yeo@gmail.com>
2019-06-11 23:42:04 +0900

    0612

---
+++ Algorithm_study/가혜랑 스터디/인구이동 (16234).cpp
@@ -0,0 +1,93 @@
+//
+//  인구이동 (16234).cpp
+//  Algorithm_study
+//
+//  Created by Yeo Kyu Li on 11/06/2019.
+//  Copyright © 2019 Yeo Kyu Li. All rights reserved.
+//
+
+#include <iostream>
+#include <cstring>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+int N, L, R;
+int arr[101][101];
+bool visited[101][101];
+int ppl, national;
+queue<pair<int,int>> close_natioanl;
+
+void BFS(int a, int b){
+    int dx[] = {0,0,1,-1};
+    int dy[] = {1,-1,0,0};
+    queue<pair<int,int>> q;
+    ppl = arr[a][b];
+    visited[a][b]= true;
+    q.push(make_pair(a, b));
+    close_natioanl.push(make_pair(a, b));
+    while(!q.empty()){
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+        for(int i = 0 ; i < 4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            
+            int diff = abs(arr[a][b]-arr[nx][ny]);
+            if(nx >= 0 && ny >= 0 && nx < N && ny < N && L <= diff && diff <= R && !visited[nx][ny]){
+                visited[nx][ny]= true;
+                ppl += arr[nx][ny];
+                national +=1;
+                close_natioanl.push(make_pair(nx, ny));
+                q.push(make_pair(nx,ny));
+            }
+            
+        }
+        
+        
+    }
+    
+    
+}
+
+int main()
+{
+    cin >> N >> L >> R;
+    
+    for(int i = 0 ; i < N; i++){
+        for(int j = 0 ; j < N; j++){
+            cin >> arr[i][j];
+        }
+    }
+    
+    int movePpl = 0;
+    while(1){
+        bool found = false;
+        memset(visited, false, sizeof(visited));
+        for(int i = 0 ; i < N; i++){
+            for(int j = 0 ; j < N; j++){
+                if(visited[i][j])
+                    continue;
+                national = 1;
+                BFS(i,j);
+                
+                if(national >= 2){
+                    found = true;
+                    int avr = ppl /close_natioanl.size();
+                    while(!close_natioanl.empty()){
+                        arr[close_natioanl.front().first][close_natioanl.front().second] = avr;
+                        close_natioanl.pop();
+                    }
+                }
+            }
+        }
+        if(found)
+            movePpl++;
+        else
+            break;
+    }
+    cout << movePpl << endl;
+    
+    return 0;
+}
--- Algorithm_study/해싱/해쉬 - array.cpp
+++ Algorithm_study/해싱/해쉬 - array.cpp
@@ -28,3 +28,206 @@ struct HashTable{
     struct HashNode** table;
 };
 
+struct HashTable* CreateHashTable(int tableSize);
+int GetKey(int data, int tableSzie);
+bool HashSearch(struct HashTable* hTable, int data);
+void HashInsert(struct HashTable* hTable, int data);
+void HashDelete(struct HashTable* hTable, int data);
+void DeleteHashTable(struct HashTable* hTable);
+void PrintHashTable(struct HashTable* hTable);
+struct HashTable* ReHash(struct HashTable* hTable);
+
+int main(void){
+    int tableSize;
+    cout << "Table Size를 입력: ";
+    cin >> tableSize;
+    //(1) HashTable 생성
+    struct HashTable* hTable = CreateHashTable(tableSize);
+    //(2) HashTable 요소 삽입
+    cout << "데이터 삽입 (0-99)" << endl;
+    for(int i = 0 ; i < 100; i++){
+        HashInsert(hTable, i);
+    }
+    //(3) HashTable 출력
+    PrintHashTable(hTable);
+    
+    //(4) HashTable 요소 삭제
+    cout << "모든 데이터 삭제" << endl;
+    for(int i = 0 ; i < 100; i++){
+        HashDelete(hTable, i);
+    }
+    //(5) HashTable 출력
+    PrintHashTable(hTable);
+    //(6) HashTable 소멸
+    DeleteHashTable(hTable);
+    
+    return 0;
+}
+
+struct HashTable* CreateHashTable(int tableSize){
+    //생성
+    struct HashTable* hTable;
+    hTable = (struct HashTable*)malloc(sizeof(struct HashTable));
+    
+    if(!hTable){
+        cout << "Memory Allocation Fail" << endl;
+        return NULL;
+    }
+    hTable->size = tableSize;
+    hTable->numOfData = 0;
+    hTable->table = (struct HashNode**)malloc(sizeof(struct HashNode*)*tableSize);
+    
+    if(!hTable->table){
+        cout << "Memory Allocation Fail" << endl;
+        return NULL;
+    }
+    //메모리 할당 및 초기화
+    for(int i = 0 ; i < tableSize; i++){
+        hTable->table[i] = (struct HashNode*)malloc(sizeof(struct HashNode));
+        if(!hTable->table[i]){
+            cout << "Momory Allocation Fail" << endl;
+            return NULL;
+        }
+        hTable->table[i]->key = i;
+        hTable->table[i]->count = 0;
+        hTable->table[i]->list = NULL;
+    }
+    cout << "HashTable Create" << endl;
+    return hTable;
+}
+int GetKey(int data, int tableSize){
+    return data%tableSize;
+}
+
+bool HashSearch(struct HashTable* hTable, int data){
+    struct Node* temp = NULL;
+    temp = hTable->table[GetKey(data, hTable->size)]->list;
+    
+    if(!temp){
+        return false;
+    }
+    while(temp){
+        if(temp->data==data){
+            return true;
+        }
+        temp = temp -> next;
+    }
+    return false;
+}
+
+void HashInsert(struct HashTable* hTable, int data){
+    struct HashNode* first;
+    first = hTable->table[GetKey(data,hTable->size)];
+    
+    struct Node* temp;
+    temp = first->list;
+    struct Node* node = (struct Node*)malloc(sizeof(struct Node));
+    if(!node){
+        cout << "Memory Allocation Fail" << endl;
+        return;
+    }
+    node->data=data;
+    node->next=NULL;
+    
+    //(1) Empty
+    if(!temp){
+        first->list = node;
+    }
+    //(2) Exist Duplication (앞에서 움직임)
+    else{
+        struct Node* temp2;
+        temp2 = first->list;
+        first->list = node;
+        node->next = temp2;
+    }
+    //cout << "Data Insertion Success" << endl;
+    first->count++;
+    
+    //ReHash 조건 검사
+    if((first->count / hTable->size) > LOAD_FACTOR){
+        hTable = ReHash(hTable);
+    }
+}
+void HashDelete(struct HashTable* hTable, int data){
+    struct HashNode* first;
+    first = hTable->table[GetKey(data, hTable->size)];
+    
+    struct Node* temp;
+    temp = first->list;
+    
+    struct Node* before = temp;
+    if(!temp){
+        return;
+    }
+    while(temp){
+        if(temp->data==data){
+            if(before == temp){
+                first->list = temp->next;
+            }
+            else{
+                before->next = temp->next;
+            }
+            free(temp);
+            break;
+        }
+        before = temp;
+        temp = temp->next;
+    }
+    cout << "Data Deletion Success" << endl;
+    first->count--;
+}
+void DeleteHashTable(struct HashTable* hTable){
+    for(int i = 0 ; i < hTable->size; i++){
+        struct Node* temp = NULL;
+        struct Node* before = NULL;
+        temp = hTable->table[i]->list;
+        before=temp;
+        while(temp){
+            before = temp;
+            temp = temp -> next;
+            free(before);
+        }
+    }
+    for(int i = 0 ; i < hTable->size; i++){
+        free(hTable->table[i]);
+    }
+    free(hTable);
+    cout << "HashTable Delete"<< endl;
+}
+void PrintHashTable(struct HashTable* hTable){
+    cout << "All Print Hash Table Data" << endl;
+    for(int i = 0; i < hTable->size; i++){
+        struct HashNode* first = hTable->table[i];
+        struct Node* temp = first->list;
+        cout << "Key " << i << ": ";
+        while(temp){
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+        cout << endl;
+    }
+}
+
+struct HashTable* ReHash(struct HashTable* hTable){
+    //New create
+    struct HashTable* oldTable = hTable;
+    hTable = CreateHashTable(hTable->size *2);
+    if(!hTable){
+        cout << "Memory Allocation Fail" << endl;
+        return NULL;
+    }
+    
+    //Data Copy
+    for(int i = 0 ; i < oldTable->size;i++){
+        struct HashNode* first = oldTable->table[i];
+        struct Node* temp = first->list;
+        
+        while(temp){
+            HashInsert(hTable, temp->data);
+            temp = temp->next;
+        }
+    }
+    DeleteHashTable(oldTable);
+    cout << "Rehash by Load Factor" << endl;
+    return hTable;
+}
